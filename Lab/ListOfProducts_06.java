662257a155110820105bb8e4f60968eab0ea0668
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-02-11 23:56:46 +0200

    Commit

---
+++ Lab/ListOfProducts_06.java
@@ -0,0 +1,26 @@
+package Lists.Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ListOfProducts_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int products = Integer.parseInt(scanner.nextLine());
+
+        List<String> productsList = new ArrayList<>();
+        for (int i = 1; i <= products; i++) {
+            String currentProduct = scanner.nextLine();
+            productsList.add(currentProduct);
+        }
+        List<String> sortedList = productsList.stream().sorted().collect(Collectors.toList());
+        int article = 0;
+        for (String product : sortedList) {
+            article++;
+            System.out.printf("%d.%s\n", article, product);
+        }
+    }
+}
